2d7b1680da297cebffd6e5dcd88a592acb6b1ec4
rhaclley <58818041+rhaclley@users.noreply.github.com>
2024-01-11 15:58:29 -0300

    views, templates and dictionary

--- appblog/views.py
+++ appblog/views.py
@@ -3,21 +3,6 @@ from .models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        'author': 'Rhaclley',
-        'title': 'Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'January 10, 2024'
-    },
-    {
-        'author': 'Rhaclley',
-        'title': 'Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'December 10, 2023'
-    }
-]
-
 def home(request):
     context = {
         'posts': Post.objects.all()
