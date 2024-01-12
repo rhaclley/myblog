503a9cda3fbe8e0be2a905ae5e03fb6bb56bc660
rhaclley <58818041+rhaclley@users.noreply.github.com>
2024-01-11 23:20:32 -0300

    users views

--- myblog/settings.py
+++ myblog/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'appblog.apps.AppblogConfig',
-    'user.app.UsersConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
