5c62c96a8192df710c8ac253fa17342a15cec48e
rhaclley <58818041+rhaclley@users.noreply.github.com>
2024-01-22 18:38:53 -0300

    email and pass recovery

--- myblog/urls.py
+++ myblog/urls.py
@@ -27,6 +27,21 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', user_views.logout_view, name='logout'),
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+        name='password_reset'
+        ),
+    path(
+        'password-reset-confirm/done/',
+        auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        name='password_reset_done'
+        ),
+    path(
+        'password-reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+        name='password_reset_confirm'
+        ),
     path('', include('appblog.urls')),
 ]
 
